680aa197aeb50fa92462237a29e4fc5d3becb7e4
Joanna Czubasiewicz <joanna.czubasiewicz@gmail.com>
2019-07-05 11:59:48 +0200

    Add game result options to script.js

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -26,16 +26,48 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+/*let playerMove = 'nieznany ruch';*/
+
+
 
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
-else if (playerMove == 2){
+else if (playerInput == '2'){
     playerMove = 'kamień';
 }
-else if (playerMove == 3){
-    playerMove = "nożyce";
+else if (playerInput == '3'){
+    playerMove = 'nożyce';
 }
-
+else {
+    playerMove = 'nieznany ruch - tak nie zagramy!'
+}
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam!')
+}
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Mamy remis!')
+}
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam!')
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!')
+}
+else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Mamy remis!')
+}
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!')
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!')
+}
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Mamy remis!')
+}
